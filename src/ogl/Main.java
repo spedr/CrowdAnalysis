4b49a0c59f834aa704b3a47c1834762aecd048f6
spedr <pedro.sanvido@acad.pucrs.br>
2017-04-11 20:48:39 -0300

    Group algorithms somewhat working

--- src/ogl/Main.java
+++ src/ogl/Main.java
@@ -6,8 +6,12 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,7 +19,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		//Initialize parser with the filepath
-		Main parser = new Main("C:\\Paths_D.txt");
+		Main parser = new Main("\\Paths_D.txt");
 		
 		//Run parser
 		parser.processLineByLine();
@@ -23,14 +27,19 @@ public class Main {
 		//Operate on matrix data structures
         distanceMatrix = new double[personCounter][frameCounter][personCounter];
         Coordinate cleanMatrix[][] = new Coordinate[personCounter][frameCounter];
+        normalizedDistanceMatrix = new double[personCounter][frameCounter][personCounter];
         cleanDMatrix();
         cleanMatrix(cleanMatrix);
         fillDistanceMatrix();
+        fillNormalizedDistanceMatrix();
+        O_ALGORITMO();
+        dAuxStructSorting();
+        navigateMap();
+        System.out.println(output);
+        System.out.println(normalizedDistanceMatrix[9][0][10]);
+        System.out.println(normalizedDistanceMatrix[9][40][10]); //0.067022775546908
         
-        
-		
-		
-        OpenGLInstance game = new OpenGLInstance(cleanMatrix, minX, maxX, minY, maxY, personCounter, frameCounter);
+        //OpenGLInstance game = new OpenGLInstance(cleanMatrix, minX, maxX, minY, maxY, personCounter, frameCounter);
         //game.play();
 	}
 	
@@ -83,6 +92,20 @@ public class Main {
         }
     }
     
+    public static void fillNormalizedDistanceMatrix(){
+        for(int q = 0; q<personCounter; q++){
+            for(int w = 0; w<frameCounter; w++){
+                for(int e = 0; e<personCounter; e++){
+                    if(!skipDoubles(q, e)){
+                        check.add(new Coordinate(q, e));
+                    }else {
+                    	normalizedDistanceMatrix[q][w][e] = calculateDistanceNormalized(q, e, w);
+                    }
+                }
+            }
+        }
+    }
+    
     //Aux functions
     public static double calculateDistancev2(int q, int e, int y){
         double x1 = (double) matrix[q][y].getX();
@@ -91,6 +114,80 @@ public class Main {
         double y2 = (double) matrix[e][y].getY();
         return Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
     }
+    
+    public static double calculateDistanceNormalized(int q, int e, int y){
+    	if(matrix[q][y].getX()==0 || matrix[q][y].getY()==0){
+    		return 0.0;
+    	}else{
+            double x1 = normalizeX((double) matrix[q][y].getX());
+            double y1 = normalizeY((double) matrix[q][y].getY());
+            double x2 = normalizeX((double) matrix[e][y].getX());
+            double y2 = normalizeY((double) matrix[e][y].getY());
+            return Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+    	}
+    }
+    
+    public static double normalizeX(double value){
+    	return (((double)value - (double) minX)/((double) maxX - (double) minX));
+    }
+    
+    public static double normalizeY(double value){
+    	return (((double)value - (double) minY)/((double) maxY - (double) minY));
+    }
+    
+    public static void O_ALGORITMO(){
+        for(int q = 0; q<personCounter; q++){
+            for(int w = 0; w<frameCounter; w++){
+                for(int e = 0; e<personCounter; e++){
+                	if(!skipDoubles(q,e)){
+                		//DO NOTHING
+                	}else if(q==e || normalizedDistanceMatrix[q][w][e]==0.0){
+                		//DO NOTHING
+                	}else if(normalizedDistanceMatrix[q][w][e] <= groupRange){
+                		//DO STUFF
+                		//output+="Pessoa " + q + " e pessoa " + e + " formaram um grupo na frame " + w +"\n";
+                		distanceAuxList.add(new DistanceAuxStruct(q, w, e));
+                	}
+                }
+            }
+        }
+    }
+    
+    public static void dAuxStructSorting(){
+    	for(DistanceAuxStruct i : distanceAuxList){
+    		addGroup(i.getPerson1(), i.getPerson2(),i.getFrame());
+    	}
+    	
+    }
+    
+    public static void addGroup(int p1, int p2, int frame){
+    	if(groupMap.containsKey(p1+"-"+p2)){
+    		//DO STUFF
+    		Group aux = (Group) groupMap.get(p1+"-"+p2);
+    		if(aux.getMinFrame()>frame) aux.setMinFrame(frame);
+    		if(aux.getMaxFrame()<frame) aux.setMaxFrame(frame);
+    		groupMap.put(p1+"-"+p2, aux);
+    		
+    	}else{
+    		groupMap.put(p1+"-"+p2, new Group(p1, p2, frame, frame));
+    	}
+    }
+    
+    public static void navigateMap(){
+    	Set set = groupMap.entrySet();
+    	Iterator i = set.iterator();
+    	while(i.hasNext()){
+    		Map.Entry me = (Map.Entry)i.next();
+    		Group aux = (Group) groupMap.get(me.getKey());
+    		output+="Pessoa " + aux.getPerson1() + " e pessoa "+aux.getPerson2() + "\nFrame inicial: " + aux.getMinFrame()+ "\nFrame final: "+aux.getMaxFrame() + "\n";
+    	}
+    }
+    /*
+    public static boolean addGroup(Group value){
+    	if(value.getPerson1())
+    	
+    	return false;
+    }*/
     
     //Parsing functions
     public final void processLineByLine() throws IOException {
@@ -157,12 +254,18 @@ public class Main {
 	//Var declarations
     private static Coordinate matrix[][] = new Coordinate[200][600];
     private static double distanceMatrix[][][]; //= new double[100][100][100];
+    private static double normalizedDistanceMatrix[][][];
+    private static List<DistanceAuxStruct> distanceAuxList = new ArrayList<>();
+    //private static List<Group> groupList = new ArrayList<>();
+    private static HashMap groupMap = new HashMap();
+    private static final double groupRange = 0.07;
     private static int personCounter = 0, frameCounter = 0;
     private static List<Coordinate> check = new ArrayList<>();
     private static int minX = 5000, maxX = 0, minY = 5000, maxY = 0;
     private int p = 0, t = 0;
     private final Path fFilePath;
     private final static Charset ENCODING = StandardCharsets.UTF_8;
+    private static String output = "";
     
 
 }
