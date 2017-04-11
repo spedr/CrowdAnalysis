958b427b8d9fb80342aa8215f08541b1b7121219
spedr <pedro.sanvido@acad.pucrs.br>
2017-04-11 20:50:51 -0300

    Created new structure to work on

---
+++ src/ogl/DistanceAuxStruct.java
@@ -0,0 +1,40 @@
+package ogl;
+
+public class DistanceAuxStruct {
+	private int person1, frame, person2;
+
+	public DistanceAuxStruct(int person1, int frame, int person2) {
+		super();
+		this.person1 = person1;
+		this.frame = frame;
+		this.person2 = person2;
+	}
+
+	public int getPerson1() {
+		return person1;
+	}
+
+	public void setPerson1(int person1) {
+		this.person1 = person1;
+	}
+
+	public int getFrame() {
+		return frame;
+	}
+
+	public void setFrame(int frame) {
+		this.frame = frame;
+	}
+
+	public int getPerson2() {
+		return person2;
+	}
+
+	public void setPerson2(int person2) {
+		this.person2 = person2;
+	}
+
+	
+	
+	
+}
